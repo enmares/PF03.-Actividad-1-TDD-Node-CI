3fd8b2c55f5052e87b4cc93dcaa1f210954ff794
enmares <enmares2000@gmail.com>
2025-10-29 20:33:46 +0100

    Tests acabados

---
+++ test-calcular.js
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+import { fibonacci } from '../calcular.js'
+
+describe('funcion-fibonacci', () => {
+  it('fibonacci', () => {
+    expect(5).toBe(3)
+    expect(-1).toThrow("El valor no puede ser negativo")
+    expect(1).toBe(1)
+    expect(0).toBe(0)
+  })
+})
