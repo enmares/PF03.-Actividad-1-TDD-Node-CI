a711ae5a70273d1b3d0ea01f92b166d5946227cd
enmares <enmares2000@gmail.com>
2025-10-31 15:59:36 +0100

    Empezando la función esPinValido

---
+++ src/calcular.js
@@ -0,0 +1,14 @@
+
+function esPinValido(pin){
+    
+    let resMal = -1;
+
+    if(pin == null || pin == undefined || pin.length == 0 || (pin.length >= 4 && pin.length <=6) || !Number.isNaN(pin) || ){
+        return resMal;
+    }
+}
+
+function comprobarDigitosIguales(string){
+
+    for(let i=0; )
+}
