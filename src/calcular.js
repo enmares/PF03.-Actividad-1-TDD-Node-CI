cd7dabdf0d76d6cc7ded29b2ba705bb6ee950641
enmares <enmares2000@gmail.com>
2025-11-05 19:28:41 +0100

    Función dev2 acabada

--- src/calcular.js
+++ src/calcular.js
@@ -1,14 +1,24 @@
 
 function esPinValido(pin){
     
-    let resMal = -1;
-
-    if(pin == null || pin == undefined || pin.length == 0 || (pin.length >= 4 && pin.length <=6) || !Number.isNaN(pin) || ){
-        return resMal;
+    if(pin == null || pin == undefined || pin.length == 0 || (pin.length >= 4 && pin.length <=6) || !Number.isNaN(pin) || comprobarDigitosIguales(pin) == true){
+        return true;
     }
+    return false;
 }
 
+//Comprueba si la 
 function comprobarDigitosIguales(string){
 
-    for(let i=0; )
+let aux = string[0];
+let res = true;
+
+    for(let i=1; i<string.length; i++){
+        
+        if(string[i] != aux){
+            res = false;
+            break;
+        }
+    }
+    return res;
 }
